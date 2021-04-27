d6eed385726345b7aa62f2e30ee34d3b59e4a64a
Fumika Babachan <abc@example.com>
2021-04-27 19:07:35 +0900

    下のリストコメントアウト

--- lib/main.dart
+++ lib/main.dart
@@ -117,64 +117,64 @@ class _State extends State<MyApp> {
                 ),
               ),
 
-              Padding(
-                padding: EdgeInsets.all(20.0),
-                child: TextField(
-                  controller: _todoInputController,
-                  decoration: InputDecoration(hintText: '入力してください'),
-                  autofocus: true,
-                ),
-              ),
-              Padding(
-                padding: EdgeInsets.only(
-                    top: 0.0, right: 0.0, bottom: 30.0, left: 0.0),
-                child: RaisedButton(
-                  color: Colors.teal[400],
-                  textColor: Colors.white,
-                  child: Text('保存'),
-                  onPressed: () {
-                    // 変数の変化をリアルタイムに通知する
-                    setState(
-                          () {
-                        // 何かしらの入力があるときだけ実行
-                        if (_todoInputController.text.length > 0) {
-                          // 配列に入力値を追加
-                          todoList.add(_todoInputController.text);
-                          // テキストボックスを初期化
-                          _todoInputController.clear();
-                        }
-                      },
-                    );
-                  },
-                ),
-              ),
-              Expanded(
-                child: ListView.builder(
-                  // リストの長さを計算
-                  itemCount: todoList.length,
-                  itemBuilder: (BuildContext context, int index) {
-                    return Container(
-                      padding: EdgeInsets.only(
-                          top: 0.0, right: 0.0, bottom: 0.0, left: 0.0),
-                      margin: EdgeInsets.only(
-                          top: 1.0, right: 0.0, bottom: 0.0, left: 0.0),
-                      color: Colors.cyan[600],
-                      child: ListTile(
-                        leading: Icon(Icons.star),
-                        title: Text(
-                          // リストに表示する文字列を設定
-                          ("$index : ${todoList[index]}"),
-                          style: TextStyle(
-                            fontFamily: 'OpenSans',
-                            fontSize: 24,
-                            color: Colors.white,
-                          ),
-                        ),
-                      ),
-                    );
-                  },
-                ),
-              ),
+              // Padding(
+              //   padding: EdgeInsets.all(20.0),
+              //   child: TextField(
+              //     controller: _todoInputController,
+              //     decoration: InputDecoration(hintText: '入力してください'),
+              //     autofocus: true,
+              //   ),
+              // ),
+              // Padding(
+              //   padding: EdgeInsets.only(
+              //       top: 0.0, right: 0.0, bottom: 30.0, left: 0.0),
+              //   child: RaisedButton(
+              //     color: Colors.teal[400],
+              //     textColor: Colors.white,
+              //     child: Text('保存'),
+              //     onPressed: () {
+              //       // 変数の変化をリアルタイムに通知する
+              //       setState(
+              //             () {
+              //           // 何かしらの入力があるときだけ実行
+              //           if (_todoInputController.text.length > 0) {
+              //             // 配列に入力値を追加
+              //             todoList.add(_todoInputController.text);
+              //             // テキストボックスを初期化
+              //             _todoInputController.clear();
+              //           }
+              //         },
+              //       );
+              //     },
+              //   ),
+              // ),
+              // Expanded(
+              //   child: ListView.builder(
+              //     // リストの長さを計算
+              //     itemCount: todoList.length,
+              //     itemBuilder: (BuildContext context, int index) {
+              //       return Container(
+              //         padding: EdgeInsets.only(
+              //             top: 0.0, right: 0.0, bottom: 0.0, left: 0.0),
+              //         margin: EdgeInsets.only(
+              //             top: 1.0, right: 0.0, bottom: 0.0, left: 0.0),
+              //         color: Colors.cyan[600],
+              //         child: ListTile(
+              //           leading: Icon(Icons.star),
+              //           title: Text(
+              //             // リストに表示する文字列を設定
+              //             ("$index : ${todoList[index]}"),
+              //             style: TextStyle(
+              //               fontFamily: 'OpenSans',
+              //               fontSize: 24,
+              //               color: Colors.white,
+              //             ),
+              //           ),
+              //         ),
+              //       );
+              //     },
+              //   ),
+              // ),
 
 
               // Container(
